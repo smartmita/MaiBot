7e23d52b8d95db05bd46daefe3690ab4bfa9f39d
Bakadax <bakadax@qq.com>
2025-05-14 20:39:38 +0800

    启动不报错

--- src/chat/utils/utils.py
+++ src/chat/utils/utils.py
@@ -64,7 +64,7 @@ def is_mentioned_bot_in_message(message: MessageRecv) -> tuple[bool, float]:
             )
 
     # 判断是否被@
-    if re.search(f"@[\s\S]*?（id:{global_config.BOT_QQ}）", message.processed_plain_text):
+    if re.search(f"@[\\s\\S]*?（id:{global_config.BOT_QQ}）", message.processed_plain_text):
         is_at = True
         is_mentioned = True
 
@@ -75,7 +75,7 @@ def is_mentioned_bot_in_message(message: MessageRecv) -> tuple[bool, float]:
         if not is_mentioned:
             # 判断是否被回复
             if re.match(
-                f"\[回复 [\s\S]*?\({str(global_config.BOT_QQ)}\)：[\s\S]*?]，说：", message.processed_plain_text
+                f"\\[回复 [\\s\\S]*?\\({str(global_config.BOT_QQ)}\\)：[\\s\\S]*?\\]，说：", message.processed_plain_text
             ):
                 is_mentioned = True
             else:
